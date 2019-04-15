f4d631b862358ccdc7ae1348acffd47ef299cb1c
Lyuda <lxs07a@acu.edu>
2019-04-15 15:31:07 +0300

    added react frontend

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-/* eslint-disable no-console */
-const logger = require('./logger');
-const app = require('./app');
-const port = app.get('port');
-const server = app.listen(port);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Application from './application';
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
-);
-
-server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+ReactDOM.render(
+  <Application />,
+  document.getElementById('app')
 );
